0ee8854d7b638423e790ffa8fda92ef79522d1b9
Santiago Spinetto Jung <sanjs965@gmail.com>
2020-06-28 18:29:34 -0300

    some data manipulation

---
+++ args.sh
@@ -0,0 +1,10 @@
+# Echo the first and second ARGV arguments
+echo $1 
+echo $2
+
+# Echo out the entire ARGV array
+echo $@
+
+# Echo out the size of ARGV
+echo $#
+# ejecutar el comando con argumentos (3 args) -> Bird Fish Rabbit
---
+++ grep-data.sh
@@ -0,0 +1,7 @@
+echo $1 
+
+# Cat all the files
+# Then pipe to grep using the first ARGV argument
+# Then write out to a named csv using the first ARGV argument
+
+cat data/* | grep $1 > $1.csv
